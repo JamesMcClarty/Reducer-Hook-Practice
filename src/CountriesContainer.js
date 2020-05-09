cc0fefe6a3ddd78551aa5c4e0c7e0204d8e24fb4
James McC <mariobud@gmail.com>
2020-05-09 13:44:51 -0500

    Finished, need more practice

--- src/CountriesContainer.js
+++ src/CountriesContainer.js
@@ -1,4 +1,9 @@
-import { useState, useReducer } from "react"
+import React, { useState, useEffect, useRef, useReducer } from "react";
+import CountryCard from "./CountryCard";
+import AdvancedFilter from "./AdvancedFilter";
+
+import { getAllCountries } from "./calls/countries";
+
 
 const CountriesContainer = () => {
     const [countries, setCountries] = useState([]);
@@ -12,8 +17,54 @@ const CountriesContainer = () => {
         }
     );
 
+    useEffect(() => {
+        getAllCountries()
+          .then(response => {
+            if (componentIsMounted.current) {
+              setCountries(response);
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+        return () => {
+          componentIsMounted.current = false;
+        };
+      }, []);
+
     const handleFilterCountries = event =>{
         const {name, value} = event.target;
         setFilterInput({[name]: value});
     }
-}
+
+    const filterCountries = list =>{
+        return list.filter(item => {
+            return(item.name
+                .toLowerCase()
+                .includes(
+                    filterInput.name.toLowerCase()
+                )
+                &&
+                item.population >= filterInput.population
+                );
+        });
+    };
+
+    const countriesList = filterCountries(countries);
+
+    return (
+        <>
+          <AdvancedFilter
+            searchValue={filterInput}
+            handleChangeValue={handleFilterCountries}
+          />
+          <div className="countries-container">
+            {countriesList.map(country => (
+              <CountryCard key={country.numericCode} country={country} />
+            ))}
+          </div>
+        </>
+      );
+};
+
+export default CountriesContainer;
